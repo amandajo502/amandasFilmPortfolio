ef7fd44b1623d97078ea6493104f3060874260a6
Amanda Cox <amandacoximages@gmail.com>
2022-07-19 20:13:58 -0400

    JavaScript Feature

---
+++ js.js
@@ -0,0 +1,55 @@
+document.getElementById("btn1"),addEventListener("click", function()
+
+{
+    var gallery1=document.getElementById("nature");
+    if (gallery1.style.display=="none")
+    {
+gallery1.style.display = "block";
+    }
+    else
+    {
+        gallery1.style.display="none";
+    }
+})
+
+document.getElementById("btn2"),addEventListener("click", function()
+
+{
+    var gallery2=document.getElementById("sky");
+    if (gallery2.style.display=="none")
+    {
+gallery2.style.display = "block";
+    }
+    else
+    {
+        gallery2.style.display="none";
+    }
+})
+
+document.getElementById("btn3"),addEventListener("click", function()
+
+{
+    var gallery3=document.getElementById("portraits");
+    if (gallery3.style.display=="none")
+    {
+gallery3.style.display = "block";
+    }
+    else
+    {
+        gallery3.style.display="none";
+    }
+})
+
+document.getElementById("btn4"),addEventListener("click", function()
+
+{
+    var gallery4=document.getElementById("collage");
+    if (gallery4.style.display=="none")
+    {
+gallery4.style.display = "block";
+    }
+    else
+    {
+        gallery4.style.display="none";
+    }
+})
